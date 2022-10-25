d09cfd036b649d3fdfd3df2fab1df948932e6b35
Mason Wheeler <mwheel@uw.edu>
2022-10-25 12:54:45 -0700

    lab3

---
+++ EE201Lab.ino
@@ -0,0 +1,27 @@
+// https://wokwi.com/projects/346520820422541907
+
+#define POT A0
+#define LED 13
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(POT, INPUT);
+  pinMode(LED, OUTPUT);
+}
+int rate = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  rate = analogRead(POT);
+  Serial.println(analogRead(POT));
+  if(rate < 200) {
+    rate = 200;
+  }
+  if (rate > 950) {
+    rate = 0;
+  }
+  digitalWrite(LED, HIGH);
+  delay(rate);
+  digitalWrite(LED, LOW);
+  delay(rate);
+}
